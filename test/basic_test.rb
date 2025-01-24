6ea3b520119f1520b9331c762de8050a68b24d45
Nathan Jaremko <nathan@jaremko.ca>
2025-01-24 14:55:37 -0500

    Fix bad test

--- test/basic_test.rb
+++ test/basic_test.rb
@@ -667,7 +667,7 @@ class BasicTest < Minitest::Test
     StringIO.new(csv_content).tap { |io| OSV.for_each(io, ignore_null_bytes: true) { |row| actual << row } }
     assert_equal expected, actual
 
-    actual = OSV.for_each(csv_content, ignore_null_bytes: true).to_a
+    actual = OSV.for_each(StringIO.new(csv_content), ignore_null_bytes: true).to_a
     assert_equal expected, actual
 
     # Without ignore_null_bytes, null bytes are preserved
